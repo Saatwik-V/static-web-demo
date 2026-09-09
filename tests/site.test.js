6f01ae10dedfd8be6bc48c66dc6996c12d64bf40
V.Saatwik <saatwik455@gmail.com>
2026-09-09 12:20:45 +0530

    b

--- tests/site.test.js
+++ tests/site.test.js
@@ -15,7 +15,7 @@ const requiredTabs = [
   'Software Engineering',
   'CI/CD Pipeline',
   'Instructable',
-  'Tab to fail the pipeline'
+  'Tab to fail the pipeline',
 ];
 
 requiredTabs.forEach(tabName => {
